ed115a50e4c159e508c9adedfc8bbfc63188415f
Krichanov Sergey <dsirax@icloud.com>
2024-02-09 08:51:18 +0100

    fix tests wiki

--- tests/test_wiki_android.py
+++ tests/test_wiki_android.py
@@ -3,7 +3,7 @@ from selene import browser, have
 from allure import step
 
 
-def test_open_article(mobile_management_android):
+def test_open_article_python(mobile_management_android):
     with step('Open article'):
         browser.element((AppiumBy.ACCESSIBILITY_ID, 'Search Wikipedia')).click()
         browser.element((AppiumBy.ID, 'org.wikipedia.alpha:id/search_src_text')).type(
@@ -14,3 +14,14 @@ def test_open_article(mobile_management_android):
         result = browser.all((AppiumBy.ID, 'org.wikipedia.alpha:id/page_list_item_title'))
         result.should(have.size_greater_than(0))
         result.first.should(have.text('Python'))
+
+
+def test_open_article_wiki(mobile_management_android):
+    with step('Type search'):
+        browser.element((AppiumBy.ACCESSIBILITY_ID, "Search Wikipedia")).click()
+        browser.element((AppiumBy.ID, "org.wikipedia.alpha:id/search_src_text")).type('Wiki')
+
+    with step('Verify content found'):
+        results = browser.all((AppiumBy.ID, 'org.wikipedia.alpha:id/page_list_item_title'))
+        results.should(have.size_greater_than(0))
+        results.first.should(have.text('Wiki'))
